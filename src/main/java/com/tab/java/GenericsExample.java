7b4c5beb18bb15daf43d7bb0767442b11ae35704
Tabrez2 <72229863+Tabrez2@users.noreply.github.com>
2024-09-09 00:59:38 +0530

    Generics in core java

---
+++ src/main/java/com/tab/java/GenericsExample.java
@@ -0,0 +1,14 @@
+package com.tab.java;
+
+public class GenericsExample {
+
+	public static void main(String[] args) {
+
+//		IntegerPrinter printer= new IntegerPrinter(36);
+//		printer.print();
+		
+		DoublePrinter printer = new DoublePrinter(6.9);
+		printer.print();
+	}
+
+}
